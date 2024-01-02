71addc56175ac2959831e730e7a752a5c6e0c8d3
Beata Konya <beatakonya@gmail.com>
2024-01-02 10:19:10 +0000

    added questions and answers for the quiz

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,7 +1,24 @@
 var questions = [
     {
-        title:
-        choices:
-        answer:
+        title: "Who invented the Rubik's Cube?",
+        choices:"Steve Jobs", "Marie Curie", "Erno Rubik", "Thomas Edison",
+        answer: "Erno Rubik"
+    },
+    {
+        title: "What is the name of the scientist who discovered Vitamin C?",
+        choices: "Albert Szent-Gyorgyi", "Isaac Newton", "James Watt", "Laszlo Biro",
+        answer: "Albert Szent-Gyorgyi"
+
+    },
+    {
+        title: "Who is the 'Father of Modern Computer'?",
+        choices: "Sigmund Freud", "Janos Neumann", "Bill Gates", "Nikola Tesla",
+        answer: "Janos Neumann"
+    },
+    {
+        title: "What is the name of the scientist who developed the holographic theory?",
+        choices: "Albert Einstein", "Niels Bohr", "Alan Turing", "Dennis Gabor",
+        answer: "Dennis Gabor"
+
     }
-]
+];
