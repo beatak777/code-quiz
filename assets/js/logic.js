3be1f04cbaa069011373a1adac42a64d5b8312c0
Beata Konya <beatakonya@gmail.com>
2024-01-02 14:55:12 +0000

    noted and fixed code error in questions js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,47 +5,76 @@ const questionTitle = document.querySelector('#question-title');
 const choices = document.querySelector('#choices');
 const feedback = document.querySelector('#feedback');
 const timer = document.querySelector('#time');
-const finalScore= document.querySelector('#final-score');
+const finalScore = document.querySelector('#final-score');
 const endScrn = document.querySelector('#end-screen');
 const submit = document.querySelector('#submit');
 const initials = document.querySelector('#initials');
 
+var intervalHandle;
+var currentQuestionIndex = 0;
+var time = 60;
+var timerId;
+
+function clockTimer() {
+    time--;
+
+    timer.textContent = time
+    if (time === 0 || currentQuestionIndex === 5) {
+        clearInterval(timerId);
+        quizFinish();
+    }
+}
 
 function displayQuestions() {
 
+    if (currentQuestionIndex === 5) {
+        questionScrn.classList.add("hide");
+        endScrn.classList.remove("hide");
+        return
+    }
 }
 
-/  / Set of questions --> array of objects
-// Each question needs the following:
-  // Question text
-  // Set of answers
-  // Which answer is correct
+const currentQuestionIndex = questions[currentQuestionIndex];
 
-// Landing page:
-  // Explanation of the quiz
-  // Start button
+questionTitle.innerText = currentQuestion.title;
+choices.innerHTML = '';
+
+
 
 // Click the start button:
-  // Landing page goes away
-  // Timer starts
-  // The first question appears (with its answers)
+// Landing page goes away
+// Timer starts
+// The first question appears (with its answers)
 
 // For each question:
-  // User clicks an answer
-  // Their choice is compared to the correct answer as stored in the question's object
-  // If correct, tell them
-  // If incorrect, tell them AND subtract time from the timer
-  // Optional: play a sound for correct or incorrect
-  // Either way, the question disappears after a few seconds and the next question appears
+// User clicks an answer
+// Their choice is compared to the correct answer as stored in the question's object
+// If correct, tell them
+// If incorrect, tell them AND subtract time from the timer
+// Optional: play a sound for correct or incorrect
+// Either way, the question disappears after a few seconds and the next question appears
 
 // After the last question:
-  // Timer stops
-  // Question disappears
-  // Form appears for user to enter their initials
-  // Display their score
+// Timer stops
+// Question disappears
+// Form appears for user to enter their initials
+// Display their score
 
 // User submits form
-  // Initials and score get stored in local storage
-  // User is taken to the high scores page
-  // High scores are listed, sorted highest to lowest
-  // User has option to take the quiz again
+// Initials and score get stored in local storage
+// User is taken to the high scores page
+// High scores are listed, sorted highest to lowest
+// User has option to take the quiz again
+
+
+// these are done 
+
+// Set of questions --> array of objects 
+// Each question needs the following:
+// Question text
+// Set of answers
+// Which answer is correct
+
+// Landing page:
+// Explanation of the quiz
+// Start button
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,29 +1,29 @@
 var questions = [
     {
         title: "Who invented the Rubik's Cube?",
-        choices: "Steve Jobs", "Marie Curie", "Erno Rubik", "Thomas Edison",
+        choices: ["Steve Jobs", "Marie Curie", "Erno Rubik", "Thomas Edison"],
         answer: "Erno Rubik"
     },
     {
         title: "What is the name of the scientist who discovered Vitamin C?",
-        choices: "Albert Szent-Gyorgyi", "Isaac Newton", "James Watt", "Laszlo Biro",
+        choices: ["Albert Szent-Gyorgyi", "Isaac Newton", "James Watt", "Laszlo Biro"],
         answer: "Albert Szent-Gyorgyi"
 
     },
     {
         title: "Who is the 'Father of Modern Computer'?",
-        choices: "Sigmund Freud", "Janos Neumann", "Bill Gates", "Nikola Tesla",
+        choices: ["Sigmund Freud", "Janos Neumann", "Bill Gates", "Nikola Tesla"],
         answer: "Janos Neumann"
     },
     {
         title: "What is the name of the scientist who developed the holographic theory?",
-        choices: "Albert Einstein", "Niels Bohr", "Alan Turing", "Dennis Gabor",
+        choices: ["Albert Einstein", "Niels Bohr", "Alan Turing", "Dennis Gabor"],
         answer: "Dennis Gabor"
 
     },
     {
         title: "What is the name of the engineer who invented the Ford T model?",
-        choices: "Harrison Ford", "Jozsef Galamb", "Henry Ford", "Arthur Paul Stern",
+        choices: ["Harrison Ford", "Jozsef Galamb", "Henry Ford", "Arthur Paul Stern"],
         answer: "Jozsef Galamb"
 
     }
